8ad35426b275c52845ce3d7a9109ca92df4afc8f
Bryan Castanos <bcastanos@gmail.com>
2016-03-14 10:55:28 -0700

    local strategy

---
+++ config/auth.js
@@ -0,0 +1,12 @@
+// TWITTER AUTH 
+
+// module.exports = {
+//   'twitter': {
+//     'clientID': 'asdfasdf',
+//     'clientSecret' : 'asdfasdfsd',
+//     'callbackURL' : 'http://localhost:3000/auth/twitter/callback'
+//     'profileFields': ['emails', 'displayName']
+//   }
+// }
+
+// PUT CLIENTID AND SECRET IN .ENV TO ENSURE SECURITY

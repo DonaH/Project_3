34f534d1fcded660e8a7dc6386a27b517ec98bfb
dona <dona.hsieh@gmail.com>
2016-03-20 21:23:58 -0700

    added hamburger menu for small screen and mobile view

--- public/script/script.js
+++ public/script/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
   $('.parallax').parallax();
   $('.dropdown-button').dropdown();
+  // activates the side menu - hamburger
+  $(".button-collapse").sideNav();
+  // $('.collapsible').collapsible();
+
 });
